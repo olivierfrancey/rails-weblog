9d6ed4e577e98cb31743b31b535590059505011e
Thibaud <thibaud@thibaud.gg>
2017-06-08 16:55:53 +0000

    Exercice1 ok <--- Olivier

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#display_name when admin' do
+    user = User.new(admin: true, name: 'bob')
+    assert_equal('bob (admin)', user.display_name, msg = user.name + ' is wrongly displayed')
+  end
+  
+  test '#display_name when not_admin' do
+    user = User.new(admin: false, name: 'jane')
+    assert_equal('jane', user.display_name, msg = user.name + ' is wrongly displayed')
+  end
 end
