d29723391dcd3d6fc20492fedd8c65798d02a5b9
Thibaud <thibaud@thibaud.gg>
2017-06-08 17:08:47 +0000

    Exercice 1 encore OK <---- Olivier

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,12 +2,14 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test '#display_name when admin' do
-    user = User.new(admin: true, name: 'bob')
-    assert_equal('bob (admin)', user.display_name, msg = user.name + ' is wrongly displayed')
+    #user = users(:bob)
+    #user = User.new(admin: true, name: 'bob')
+    assert_equal('Bob (admin)', users(:bob).display_name, msg = 'Bob is wrongly displayed')
   end
   
   test '#display_name when not_admin' do
-    user = User.new(admin: false, name: 'jane')
-    assert_equal('jane', user.display_name, msg = user.name + ' is wrongly displayed')
+    #user = User.new(admin: false, name: 'jane')
+    user = users(:jane)
+    assert_equal('Jane', users(:jane).display_name, msg = 'Jane is wrongly displayed')
   end
 end
